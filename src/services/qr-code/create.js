7c56b6b5dfcd6a80594e79aa4032f543be3a8cf8
BiancaMaria <bianca.engcomp@gmail.com>
2025-08-08 11:20:02 -0300

    Definição dos objetos que compõe o Qrcode

--- src/services/qr-code/create.js
+++ src/services/qr-code/create.js
@@ -1,8 +1,12 @@
 import prompt from 'prompt';
-import promptQrcode from '../../prompts/prompt-qrcode';
+import promptQrcode from '../../prompts/prompt-qrcode.js';
+import handle from '../qr-code/handle.js';
 
 async function createQrcode() {
 
+    prompt.get(promptQrcode,handle)
+
+    prompt.start();
 }
 
 export default createQrcode;
