c20d03334a5a3ede2d12d56e422290011f9795fe
BiancaMaria <bianca.engcomp@gmail.com>
2025-08-16 21:39:12 -0300

    Inserindo a frase splash do password

--- src/services/password/handle.js
+++ src/services/password/handle.js
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import permittedCharacters from "./utils/permitted-characters.js";
 
 async function handle() {
@@ -7,11 +8,18 @@ async function handle() {
     const passwordLength = process.env.PASSWORD_LENGTH;
     characters = await permittedCharacters();
 
+    process.stdout.write(chalk.blue("Gerando seu Password, aguarde..."));
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+  // Move o cursor para o início da linha e a limpa
+  process.stdout.write('\r\x1B[K');
     for (let i=0; i < passwordLength; i++) {
         const index = Math.floor(Math.random() * characters.length);
         password += characters[index];
     }
-        return password;
+     
+        return (chalk.green("Password gerada com sucesso!!!")+"\n"+password);
+
 }
 
 export default handle;
